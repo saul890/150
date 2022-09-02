f4c191995052f7dfb6ab841d04b0a6fff1174a3d
saulhirner890 <saulhirner@gmail.com>
2022-09-02 14:06:51 -0400

    final

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -1,2 +1,16 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+let circleBtn = document.querySelector("#circle");
+let squareBtn = document.querySelector("#square");
+circleBtn.addEventListener("click", changeCircleColor);
+squareBtn.addEventListener("click", changeSquareColor);
+function randomColors() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+function changeCircleColor() {
+    document.getElementById("circle").style.backgroundColor = randomColors();
+}
+function changeSquareColor() {
+    document.getElementById("square").style.backgroundColor = randomColors();
+}
 
 //# sourceMappingURL=index.f3bd186e.js.map
